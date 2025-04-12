f022a429d10b411da2a597a9ffbca403e43a57de
Ren-pdm0999 <ren.shibusawa0379@gmail.com>
2025-04-12 22:40:11 +0900

    Add User entity class

---
+++ src/main/java/com/knowledgenest/entity/User.java
@@ -0,0 +1,17 @@
+package com.knowledgenest.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String userName;
+    private String password;
+}
